9cd4286d0fdb559e9e63b2190ce4c9f108c8dfc9
Sam-Park <Sam.park.email@gmail.com>
2019-12-07 19:40:49 -0500

    Navbar and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import './App.css';
-
+import SurrealHeader from './img/Surreal-Header.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn to not react
-        </a>
-      </header>
+      <div className="Top-Container">
+
+      <div className="Header">
+      <img className="Header-Img" src={SurrealHeader} alt="Header"></img>
+      </div>
+      <div className="Nav-container">
+      <AnchorLink className="nav-h" href="#projects">
+              Home
+            </AnchorLink>
+            <AnchorLink className="nav-h1">Members</AnchorLink>
+            <AnchorLink className="nav-h2">Recruitment</AnchorLink>
+            <AnchorLink className="nav-h3" href="#aboutme">
+              Gallery
+            </AnchorLink>
+            <AnchorLink className="nav-h4" href="#contact">
+            Contact Us
+            </AnchorLink>
+            <AnchorLink className="nav-h4" href="#sales">
+            Sales
+            </AnchorLink>
+            <a className="nav-h4" href="https://classic.warcraftlogs.com/guild/us/pagle/surreal" target="_blank" rel="noopener noreferrer">
+              Warcraft Logs
+            </a>
+      </div>
+      </div>
+      <div className="Landing-Container">
+        
+      </div>
     </div>
   );
 }
